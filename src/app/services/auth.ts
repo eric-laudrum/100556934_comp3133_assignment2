05f527641954f64389e03b0456e1e6deb2a4f209
eric-laudrum <ericlaudrum@gmail.com>
2026-04-07 16:42:15 -0400

    update: auth service to store token

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { map } from 'rxjs/operators';
+
+const LOGIN_QUERY = gql`
+  query Login($username: String, $password: String!) {
+      login(username: $username, password: $password) {
+        status
+        message
+        token
+      }
+    }
+`;
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  constructor(private apollo: Apollo){}
+
+  login( credentials: any ){
+    return this.apollo.query<any>({
+      query: LOGIN_QUERY,
+      variables: credentials
+    }).pipe(
+      map(result => result.data.login)
+    );
+  }
+}
