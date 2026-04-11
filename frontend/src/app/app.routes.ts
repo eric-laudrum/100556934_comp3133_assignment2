87715d8a8928df1772ab82fafc3b1119089d2bf2
eric-laudrum <ericlaudrum@gmail.com>
2026-04-11 12:28:56 -0400

    update: routes to add employee

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { SignUp } from './components/signup/signup';
 import { EmployeeList } from './components/employee-list/employee-list'
+import { EmployeeAdd } from './components/employee-add/employee-add';
 
 
 
@@ -10,6 +11,7 @@ export const routes: Routes = [
     { path: 'login', component: Login },
     { path: 'signup', component: SignUp },
     { path: 'employees', component: EmployeeList },
+    { path: 'employees/add', component: EmployeeAdd},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login' }
 ];
